622bb92b5c59fd18dfd01a13fe4bce8b99aa3fab
Joshua Omobola <omobolathejoshua@gmail.com>
2024-09-30 04:21:22 +0100

    init NothingFactory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        'ibm-mono': ["IBM Plex Mono", "monospace"]
+      }
     },
   },
   plugins: [],
